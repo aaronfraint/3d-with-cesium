247ba8047aa6bb71458cfcf07d48996f0df22d24
Aaron Fraint <aaronfraint@icloud.com>
2025-02-10 10:01:49 -0500

    remove react deps

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
 import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import EnvironmentPlugin from "vite-plugin-environment";
 
